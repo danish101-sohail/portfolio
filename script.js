05adb77e54ff85d7c4157996cbd4b9e229c743b5
danish101-sohail <danishsohail12911@gmail.com>
2023-12-30 04:31:53 +0530

    update main

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function togglemenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+  
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Full Stack Web Developer";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Software Engineer";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "Web Designer";
+    }, 8000); //1s = 1000 milliseconds
+}
+
+textLoad();
+setInterval(textLoad, 12000);
